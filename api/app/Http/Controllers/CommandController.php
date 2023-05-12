c99d1d21c3712341496ff3a61117b1cd95a2e303
Kamzen <kamzen1994@gmail.com>
2023-05-12 10:19:21 +0200

    Update CommandController.php

--- api/app/Http/Controllers/CommandController.php
+++ api/app/Http/Controllers/CommandController.php
@@ -2,24 +2,132 @@
 
 namespace App\Http\Controllers;
 
+use App\Command;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class CommandController extends Controller
 {
     //
-    public function createCommand(Request $request){
+    public function createCommand(Request $request)
+    {
 
-        $validate = Validator::make($request->all(), [
-            'platform' => 'required',
-            'operating_system' => 'required',
-            'command' => 'required',
-            'description' => 'required'
-        ]);
+        try {
 
-        if($validate->fails()){
-            
+            $validate = Validator::make($request->all(), [
+                'platform' => 'required',
+                'operating_system' => 'required',
+                'command' => 'required',
+                'description' => 'required'
+            ]);
+
+            if ($validate->fails()) {
+                return response()->json([
+                    'errors' => $validate->errors()
+                ], 422);
+            }
+
+            $command = Command::create($request->all());
+
+            if (!$command) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Error creating command'
+                ]);
+            }
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Command created successfully',
+                'command' => $command->get()->first()
+            ]);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Internal server error'
+            ]);
         }
 
+    }
+
+    public function updateCommand(Request $request)
+    {
+        try {
+
+            $commandId = $request->route()->parameter('command_id');
+            $command = DB::table('commands')->where('id', $commandId);
+
+            if ($command->count() <= 0) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Command not found, please try again'
+                ]);
+            }
+            $command->update($request->all());
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Command updated successfully',
+                'command' => $command->first()
+            ]);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Internal server error'
+            ]);
+        }
+    }
+
+    public function deleteCommand(Request $request)
+    {
+        try {
+
+            $commandId = $request->route()->parameter('command_id');
+
+            $command = DB::table('commands')->where('id', $commandId);
+
+            if($command->count() <= 0){
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Error deleting command, please try again'
+                ]);
+            }
+
+            $command->delete();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Command deleted successfully',
+            ]);
+
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Internal server error'
+            ]);
+        }
+    }
+
+    public function getAllCommands()
+    {
+        try {
+
+            $commands = Command::all();
+
+            return response()->json([
+                'success' => true,
+                'commands' => $commands
+            ]);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Internal server error'
+            ]);
+        }
     }
 }
